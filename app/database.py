23367e89f7a4ea7c7cfad25086de7d08812a9880
mikhail <tihonov.mv.2006@gmail.com>
2026-06-13 18:14:07 +0300

    Implementing models.py and database.py

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from typing import AsyncGenerator
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
