da13dfd087015ba0eec588be5d01a1971c10aacb
mikhail <tihonov.mv.2006@gmail.com>
2026-07-10 11:30:18 +0300

    Add otp to DataType enum and migration 0005

---
+++ alembic/versions/0005_add_otp_to_datatype.py
@@ -0,0 +1,32 @@
+"""add otp value to datatype enum
+
+Revision ID: 0005
+Revises: 0004
+Create Date: 2026-07-10
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+revision: str = "0005"
+down_revision: Union[str, None] = "0004"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("ALTER TYPE datatype ADD VALUE IF NOT EXISTS 'otp'")
+
+
+def downgrade() -> None:
+    op.execute(
+        """
+        ALTER TYPE datatype RENAME TO datatype_old;
+        CREATE TYPE datatype AS ENUM ('password', 'card', 'text', 'binary');
+        ALTER TABLE items
+            ALTER COLUMN type TYPE datatype
+            USING type::text::datatype;
+        DROP TYPE datatype_old;
+    """
+    )
